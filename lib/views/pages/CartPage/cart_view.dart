5653bdf60df0376937fff66da3628a1f8b67eeff
marcoemad02 <emadmarko18@gmail.com>
2023-08-21 17:20:51 +0300

    show the cart is empty TEXT

--- lib/views/pages/CartPage/cart_view.dart
+++ lib/views/pages/CartPage/cart_view.dart
@@ -1,35 +1,79 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 import 'package:flutter/material.dart';
-import 'package:souchi/views/pages/CartPage/elements/body.dart';
-
-import 'package:souchi/views/widgets/checkout_buttom.dart';
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:souchi/views/widgets/second_appBar.dart';
-
-import '../../../enums.dart';
+import 'package:souchi/enums.dart';
 import '../../widgets/NavBar.dart';
+import '../../widgets/checkout_buttom.dart';
+import 'elements/body.dart'; // Import your other components
 
 class CartScreen extends StatelessWidget {
- const CartScreen({super.key, required this.streamBranch, required this.branchName, required this.branchId});
+  const CartScreen({
+    Key? key,
+    required this.streamBranch,
+    required this.branchName,
+    required this.branchId,
+  }) : super(key: key);
+
   final Stream<QuerySnapshot> streamBranch;
   final String branchName;
   final int branchId;
- // final controller=Get.put(ProductController());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: SecondAppBar(streamBranch:streamBranch,branchId: branchId,branchName: branchName, ),
-      body: Column(
-        children: [
-          Expanded(child: Body(branchID: branchId, )),
-          const SizedBox(height: 20,),
-          CustomCheckoutButton(branchId: branchId,),
-          const SizedBox(height: 20,),
-        ],
-      ) ,
+      appBar: SecondAppBar(
+        streamBranch: streamBranch,
+        branchId: branchId,
+        branchName: branchName,
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: streamBranch,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator(); // Loading indicator while waiting for data
+          }
 
-      bottomNavigationBar:  CustomBottomNavBar(selectedMenu: MenuState.cart,streamBranch: streamBranch,branchId: branchId,branchName: branchName),
+          bool isCartEmpty = snapshot.data?.docs.isEmpty ?? true;
+
+          return Column(
+            children: [
+              Expanded(
+                child: Body(branchID: branchId),
+              ),
+              const SizedBox(height: 20),
+              if (!isCartEmpty)
+                CustomCheckoutButton(branchId: branchId),
+              if (isCartEmpty)
+                const IfCartEmptyWidget(), // Show if cart is empty
+              const SizedBox(height: 20),
+            ],
+          );
+        },
+      ),
+      bottomNavigationBar: CustomBottomNavBar(
+        selectedMenu: MenuState.cart,
+        streamBranch: streamBranch,
+        branchId: branchId,
+        branchName: branchName,
+      ),
+    );
+  }
+}
+
+class IfCartEmptyWidget extends StatelessWidget {
+  const IfCartEmptyWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Text('The  Cart  Is  Empty', style:
+        TextStyle(
+            fontSize: 22,
+            fontFamily: 'poppins',
+          color: Color(0xffFF7517)
+        )),
+      ],
     );
   }
 }
